a231bac6f9727474cebb0033c336778df5295af7
Bruno Ferreira Sergio <bruno.ferrer2@gmail.com>
2021-04-06 02:23:01 -0300

    primeira aplicação angular part I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ 
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant' }
+  leia = {name: 'Leia', isJedi: false }
+  hanSolo = {name: 'Han Solo', isJedi: false }
 }
